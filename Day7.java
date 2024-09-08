b12c0f028b313aed03f48fc462e0066f508b588a
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-08 13:04:48 +0800

    Hari 7
    variabel : inisialisasi

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		char inisial = 'W';
+		String nama ="Wahyu Anugrah";
+		String Nim ="D0224334";
+		int umur = 18;
+		double tinggi = 164.7;
+		byte berat = 85;
+		boolean mahasiswa = true;
+		
+		}
+}
