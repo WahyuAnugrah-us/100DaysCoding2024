47ee1285b3f34c68cd6e021e8ea351eed07f9145
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-09 15:32:51 +0800

    Hari 8
    variabel : update

---
+++ Day8.java
@@ -0,0 +1,25 @@
+public class Main {
+	public static void main(String[] args) {
+		char inisial = 'W';
+		inisial = 'F';
+		
+		String nama ="Wahyu Anugrah";
+		nama ="Farid Wajidi";
+		
+		String Nim ="D0224334";
+		Nim ="00000000";
+		
+		int umur = 18;
+		umur = 20;
+		
+		double tinggi = 164.7;
+		tinggi = 172.9;
+		
+		byte berat = 85;
+		berat = 63;
+		
+		boolean mahasiswa = true;
+		mahasiswa = true;
+		
+		}
+}
