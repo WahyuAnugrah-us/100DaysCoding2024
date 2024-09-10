7a745cf6630d9a2d7724e69c7ad0bec4814504f1
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-10 17:32:48 +0800

    Hari 9
    variabel : constanta(final variabel)

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		final int MINIMAL_UMUR = 18;
+		final byte MAKSIMAL_UMUR = 35;
+		final double MINIMAL_TINGGI = 165;
+		final String KODE_RUPIAH = "IDR";
+    	final char KODE_DOLLAR ='$';
+    	final boolean AKTIF =true;
+    	
+	}
+}
