671e077b2e3b31f601aa922be65bc39e7db45bae
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-14 10:40:49 +0800

    Hari 13
    Evaluasi mentor

---
+++ Day13.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+       byte x;
+        x=6;
+       System.out.println(x);
+        x=9;
+       System.out.println(x);
+    }
+    
+}
