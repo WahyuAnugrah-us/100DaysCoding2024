1599a3be97987b64b3a936821ab43b98e669ad6f
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-19 19:46:12 +0800

    hari 18
    tipe data boolean

---
+++ Day18.java
@@ -0,0 +1,11 @@
+
+public class bolean {
+    
+   public static void main (String [] args){
+       boolean mahasiswa = true;
+       System.out.println(" apakah wahyu adalah mahasiswa: " + mahasiswa);
+       
+   } 
+    
+    
+}
