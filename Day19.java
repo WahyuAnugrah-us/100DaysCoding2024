208d4bdd45906e92a9121d0ae82b8c00cb7dc7dd
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-20 19:35:54 +0800

    Hari 19
    tipe data reference : String

---
+++ Day19.java
@@ -0,0 +1,14 @@
+
+public class bolean {
+    
+   public static void main (String [] args){
+       String nama = "Wahyu Anugrah";
+       String nim = "D0224334";
+       System.out.println("Nama: "+ nama);
+       System.out.println("NIM: "+ nim);
+       
+       
+   } 
+    
+    
+}
