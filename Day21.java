a2ad56ff178031c693b2c68bcf963ce365e92cdb
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-22 20:08:07 +0800

    Hari 21
    tipe data reference Short

---
+++ Day21.java
@@ -0,0 +1,8 @@
+public class KelasBelajar {
+    
+    public static void main(String []args){
+        
+        Short jarak = 12;
+        System.out.print("Jarak rumah ke sekolah(km)"+jarak);
+    }
+}
