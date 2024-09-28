80059bcc99739e2359f55ca0d63dc647293bbc1d
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-28 19:41:19 +0800

    Hari 27
    Evaluasi : penyaringan dan icl

---
+++ Day27.java
@@ -0,0 +1,15 @@
+
+public class KatalogFilm {
+    public static void main (String[]args) {
+        System.out.println("Selamat datang di perpustakaan film favorit!");
+        final int jumlahFilm = 50;//variabel konstanta
+        String judulFilm = "iron man";
+        Short tahunRilis = 2006;
+        judulFilm = "From";
+        System.out.printf("Film berjudul %s dirilis pada tahun %d dan terdapat %d film di katalog saat ini" , judulFilm , tahunRilis , jumlahFilm);
+        
+        
+        
+    }
+    
+}
