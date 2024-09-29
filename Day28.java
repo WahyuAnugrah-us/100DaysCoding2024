7f80607432eb90191233624b15174c5305fc6ac8
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-29 19:19:57 +0800

    Hari 28
    tipe data reference : Boolean

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package reference;
+
+public class reference {
+    
+    public static void main(String[] args) {
+        Boolean pro = true ;
+        System.out.print("apakah wahyu pro player: "+pro);
+    }
+    
+}
