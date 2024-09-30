e7482740b0f6bb9595b8d74a8fad0471ae34f6c2
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-09-30 19:12:24 +0800

    Hari 29
    tipe data reference : BigInteger

---
+++ Day29.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class reference {
+    
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger("12345678901234567890");
+        System.out.print("BigInteger: " + a);;
+    }
+    
+}
