a0d1c92f3cabe3acbd30e3a4545dbcb8ecdaf100
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-01 18:42:34 +0800

    Hari 30
    tipe data reference : BigDecimal

---
+++ Day30.java
@@ -0,0 +1,11 @@
+
+import java.math.BigDecimal;
+
+public class reference {
+    
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal("1234567890.1234567890");
+        System.out.print("BigDecimal: " + a);
+    }
+    
+}
