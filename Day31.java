9e8ea2fb3f087852816b862fc25ad21ac9e341d8
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-02 18:27:28 +0800

    Hari 31
    konversi String ke tipe data primitif

---
+++ Day31.java
@@ -0,0 +1,17 @@
+public class konversi {
+    public static void main(String[]args){
+        String x ="123";
+        int y=
+        Integer.parseInt(x);
+        System.out.println(y+2);
+        String p ="123.9";
+        double q =
+        Double.parseDouble(p);
+        System.out.println(q+1.4);
+        String a ="true";
+        boolean b=
+        Boolean.parseBoolean(a);
+        System.out.println(!b);
+    }
+    
+}
