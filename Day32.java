0d4497e344bcafcb2696ba35ada565334ce9994f
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-03 18:26:54 +0800

    Hari 32
    konversi data primitif ke string

---
+++ Day32.java
@@ -0,0 +1,20 @@
+public class konversi {
+    public static void main(String[]args){
+        double a = 145.8;
+        String b =
+        String.valueOf(a);
+        
+        int x = 145;
+        String y =
+        String.valueOf(x);
+        
+        boolean p = true;
+        String q =
+        String.valueOf(p);
+        
+        char u = 'x';
+        String i =
+        String.valueOf(u);
+        System.out.println(i+q+y+b);
+    }
+}
