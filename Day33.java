5736e1d1ec36c1cd2aa30271ebb42eeac2fa17ed
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-04 19:16:57 +0800

    Hari 33
    Type Casting

---
+++ Day33.java
@@ -0,0 +1,20 @@
+public class TypeCasting {
+    
+    public static void main(String[]args){
+        double a = 92.6;
+        int b = (int)
+        a;
+        System.out.println(b);
+        float c = b;
+        System.out.println(c);
+        byte d = (byte)
+        c;
+        System.out.println(d);
+        
+        
+        
+        
+        
+    }
+    
+}
