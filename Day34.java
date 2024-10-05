7cfae96e9f3ce78457357712a7d6e99d18cb1489
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-05 21:05:29 +0800

    Hari 34
    metode input

---
+++ Day34.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[]args){
+        Scanner i = new Scanner(System.in);
+        System.out.print("apa aja terserah:");
+        String x = i.nextLine();
+        
+    }
+    
+}
