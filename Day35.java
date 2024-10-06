662a2f58828a8180baabd80488da70284f187fda
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-06 23:09:30 +0800

    Hari 35
    operator aritmatika : penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+
+public class Aritmatika {
+    
+    public static void main(String[] args){
+        
+        int a = 5;
+        int b = 8;
+        
+        int c = b + a;
+        int d = c - 8;
+        
+        
+    }
+    
+}
