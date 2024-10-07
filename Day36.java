7ee1f0fd1c9d055ce0d40adb3acb4125a6ffbcad
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-07 18:38:38 +0800

    Hari 36
    Operator aritmatika : perkalian, pembagian,dan modulo

---
+++ Day36.java
@@ -0,0 +1,14 @@
+public class Aritmatika {
+    
+    public static void main(String[] args){
+        
+        int a = 5;
+        int b = 8;
+        
+        int x = a * b;
+        int y = x / 3;
+        int z = y % 3;    
+
+    }
+    
+}
