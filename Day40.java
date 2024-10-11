2a81942d4ea6fdbe463734a17854de98db24fa40
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-11 18:42:01 +0800

    Hari 40
    operator perbandingan lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,11 @@
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 9;
+        int b = 8;
+        System.out.println(a>b);
+        a %= b;
+        System.out.println(a<b);
+    }
+    
+}
