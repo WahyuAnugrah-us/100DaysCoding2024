932b02827cb41a3f00b678a9f7b1a11cd8d65573
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-13 18:28:28 +0800

    Hari 42
    operator perbandingan sama dengan dan tidak sama dengan

---
+++ day42.java
@@ -0,0 +1,12 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        System.out.println(a==b);
+        a %= b;
+        System.out.println(a!=b);
+    }
+    
+}
