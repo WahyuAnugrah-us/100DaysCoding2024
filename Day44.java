862490e1d5339d11f657778520fb2393c07b44ec
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-15 18:43:56 +0800

    Hari 44
    operator logika: and

---
+++ Day44.java
@@ -0,0 +1,11 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        System.out.println(a>b&&a==b);
+        
+    }
+    
+}
