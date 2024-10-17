ef195917d49ff807742fd84ff8fd80f273aa6f8b
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-17 20:13:39 +0800

    Hari 46
    Operator logika : negasi/kebalikan

---
+++ Day46.java
@@ -0,0 +1,12 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        boolean c = a==b;
+        System.out.println(!c);
+        
+    }
+    
+}
