5da8b2d36c144aeb742f27388805f2c9d1158b74
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-18 20:03:10 +0800

    Hari 47
    percabangan if

---
+++ Day47.java
@@ -0,0 +1,14 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        if (a==b){
+            System.out.println("real cuy");
+        }
+        
+        
+    }
+    
+}
