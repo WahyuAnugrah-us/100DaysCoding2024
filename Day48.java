1e1103294ee1a46b156344a0a81079fd7951fbd4
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-19 20:28:06 +0800

    Hari 48
    percabangan: if-else

---
+++ Day48.java
@@ -0,0 +1,17 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        if (a==b){
+            System.out.println("real cuy");
+        }
+        else {
+            System.out.print("salah bero");
+        }
+        
+        
+    }
+    
+}
