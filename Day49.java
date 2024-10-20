f27f2bc565944a28b2f3053f4d72b6ff2a1ae0c3
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-20 18:46:09 +0800

    Hari 49
    percabangan: if,else if

---
+++ Day49.java
@@ -0,0 +1,17 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        if (a==b){
+            System.out.println("sama woy");
+        }
+        else if (a!=b){
+            System.out.print("beda cokk");
+        }
+        
+        
+    }
+    
+}
