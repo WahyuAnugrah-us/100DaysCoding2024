53c93998a6aff45c2cc2fa656ef414d9c2b7da8f
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-21 18:06:12 +0800

    Hari 50
    percabangan: if, else if, else

---
+++ Day50.java
@@ -0,0 +1,20 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 8;
+        int b = 8;
+        if (a==b){
+            System.out.println("sama woy");
+        }
+        else if (a<b){
+            System.out.print("gedean b");
+        }
+        else {
+            System.out.print("lebih besar a");
+        }
+        
+        
+    }
+    
+}
