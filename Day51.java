02a514c6c0be0cc6a9a671bb29ed70c014fd59fc
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-22 19:00:25 +0800

    Hari 51
    percabangan : switch case

---
+++ Day51.java
@@ -0,0 +1,30 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 9;
+        switch (15-a) {
+            case 1:
+            System.out.print("nilai adalah 1");
+            break;
+            case 2:
+            System.out.print("nilai adalah 2");
+            break;
+            case 3:
+            System.out.print("nilai adalah 3");
+            break;
+            case 4:
+            System.out.print("nilai adalah 4");
+            break;
+            case 5:
+            System.out.print("nilai adalah 5");
+            break;
+            case 6:
+            System.out.print("nilai adalah 6");
+            
+        }
+
+        
+    }
+    
+}
