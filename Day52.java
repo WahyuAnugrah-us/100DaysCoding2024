a59e7f0e8e89ecf57c5531d934506db679844dc1
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-23 18:31:12 +0800

    Hari 52
    ternary

---
+++ Day52.java
@@ -0,0 +1,12 @@
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 9;
+        int b = 8;
+        String c = (a>b)?"a yang paling besar":"b yang paling besar";
+        System.out.print(c);
+
+        
+    }
+    
+}
