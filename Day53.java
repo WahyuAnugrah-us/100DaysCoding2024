416929f46c232434d70afc720628e45299ff5098
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-24 16:21:07 +0800

    Hari 53
    looping: for

---
+++ Day53.java
@@ -0,0 +1,13 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 9;
+        for (int i=0;i<a;i++){
+            System.out.print(" "+i);
+        }
+
+        
+    }
+    
+}
