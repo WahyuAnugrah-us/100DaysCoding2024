87676672984186c357b838c8fa603ad38cc5e4a0
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-25 18:45:15 +0800

    hari 54
    perulangan: For Increment

---
+++ Day54.java
@@ -0,0 +1,13 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 100;
+        for (int i=0;i<a;i+=3){
+            System.out.print(" "+i);
+        }
+
+        
+    }
+    
+}
