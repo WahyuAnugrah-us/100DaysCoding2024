a764350f8901f6adc5b5edd753f6768cf300ec6e
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-26 19:15:42 +0800

    Hari 55
    perulangan For decrement

---
+++ Day55.java
@@ -0,0 +1,13 @@
+
+public class CodingDay {
+    
+    public static void main(String[] args){
+        int a = 16;
+        for (int i=100;i>a;i-=3){
+            System.out.print(" "+i);
+        }
+
+        
+    }
+    
+}
