899559f52373744046a8c305e3c84861aa997925
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-28 19:29:26 +0800

    Hari 57
    while loop increment

---
+++ Day57.java
@@ -0,0 +1,10 @@
+
+public class serah {
+    public static void main (String[]args){
+        int a = 9;
+        while (a<30){
+            System.out.println("nilai a adalah :"+a);
+            a++;
+        }
+    }
+}
