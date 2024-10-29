10db83767145af6313d296fcbda629b3eec79acf
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-29 20:40:20 +0800

    Hari 58
    while decrement

---
+++ Day58.java
@@ -0,0 +1,9 @@
+public class serah {
+    public static void main (String[]args){
+        int a = 39;
+        while (a>9){
+            System.out.println("nilai a adalah :"+a);
+            a--;
+        }
+    }
+}
