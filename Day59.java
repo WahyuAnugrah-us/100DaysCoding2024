12b6c0b221662f88e6951220fe399abc04694071
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-30 19:46:26 +0800

    Hari 59
    for each

--- Day59.java
+++ Day59.java
@@ -3,7 +3,7 @@ public class serah {
     public static void main (String[]args){
         int[]a={1,2,3,4,5};
         for(int angka:a){
-            System.out.print(angka+" ");
+            System.out.print(angka+"  ");
         }
     }
 }
