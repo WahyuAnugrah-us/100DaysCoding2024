9a7e00e4e891384c8b651bdc7796dcd4a48ce349
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-30 19:42:54 +0800

    Create Day59.java
    for each

---
+++ Day59.java
@@ -0,0 +1,9 @@
+
+public class serah {
+    public static void main (String[]args){
+        int[]a={1,2,3,4,5};
+        for(int angka:a){
+            System.out.print(angka+" ");
+        }
+    }
+}
