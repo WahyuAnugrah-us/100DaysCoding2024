c16f115314ee165674c53ab177b54c538f3d9014
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-10-31 18:50:47 +0800

    Hari 60
    do while

---
+++ Day60.java
@@ -0,0 +1,10 @@
+
+public class serah {
+    public static void main (String[]args){
+        int i = 1;
+        do {
+            System.out.println("hari ke-"+i);
+            i++;
+        } while(i<=60);
+    }
+}
