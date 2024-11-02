4b938400b6707c2253c8fbd6fd3ebd8f518363e5
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-02 21:10:37 +0800

    Hari 62
    Keyword break

---
+++ Day62.java
@@ -0,0 +1,11 @@
+
+public class serah {
+    public static void main (String[]args){
+        for (int i = 0;i>=0;i++){
+            System.out.println("perulangan ke-"+i);
+            if (i==10){
+               break; 
+            }
+        }
+    }
+}
