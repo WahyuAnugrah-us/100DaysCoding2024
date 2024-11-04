2001708edb7af956353d2100ce1fb570e42285d1
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-04 20:27:21 +0800

    Hari 64
    break label

---
+++ Day64.java
@@ -0,0 +1,18 @@
+
+public class serah {
+    public static void main (String[]args){
+             x:
+             for(int i=0;i>=0;i++){
+                      System.out.println("Level up");
+                      if(i==999){
+                               System.out.println("level"+i+"telah dicapai");
+                               System.out.println("anda mencapai batas untuk manusia");
+                               System.out.println("opsi untuk menjadi mahluk transcendent");
+                               System.out.println("hancurkan batas level");
+                               System.out.println("dapatkan Keilahian fallen GOD");
+                               break x;
+                      }
+                      
+             }
+    }
+}
