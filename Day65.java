b582dc198b8b12438703b82de38b0ace0c3e5ece
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-05 19:16:18 +0800

    hari 65
    nested loop

---
+++ Day65.java
@@ -0,0 +1,12 @@
+
+public class serah {
+    public static void main (String[]args){
+             
+             for(int i=0;i<=1000;i++){
+                      for(int j=i;j>=-1000;j--){
+                               System.out.print(j+" ");
+                      }
+                      
+             }
+    }
+}
