1a52c1b3f32a3f108dc4bd879bc31b3cd43c2dee
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-06 19:29:53 +0800

    Hari 66
    pola persegi dan persegi panjang

---
+++ Day66.java
@@ -0,0 +1,19 @@
+
+public class serah {
+    public static void main (String[]args){
+             for(int i=0;i<=7;i++){
+                      for(int j=0;j<=7;j++){
+                               System.out.print("1 ");
+                      }
+                      System.out.println();
+             }
+             System.out.println();
+             
+             for(int i=0;i<=4;i++){
+                      for(int j=0;j<=7;j++){
+                               System.out.print("2 ");
+                      }
+                      System.out.println();
+             }
+    }
+}
