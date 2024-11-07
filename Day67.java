bb0ddf8879f29c100f97478161553a279fee65cc
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-07 19:53:23 +0800

    Hari 67
    method void

---
+++ Day67.java
@@ -0,0 +1,10 @@
+
+public class serah {
+         public void x(){
+                  System.out.print("apa aja deh");
+         }
+    public static void main (String[]args){
+             serah y = new serah();
+             y.x();
+    }
+}
