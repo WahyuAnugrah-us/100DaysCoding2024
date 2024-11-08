32cbac2ca4db079b59d95eae1d6f8d1dbf1799de
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-08 22:14:17 +0800

    Hari 68
    Method Non-void

---
+++ Day68.java
@@ -0,0 +1,12 @@
+
+public class serah {
+         public double x(double a,double b){
+                  return a+b;
+         }
+    public static void main (String[]args){
+             serah y = new serah();
+             double z = y.x(12.25,12.75);
+             
+             System.out.print(z);
+    }
+}
