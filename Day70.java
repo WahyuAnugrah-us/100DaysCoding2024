416d6311b7d5dc4757a847be1162cc4bc6664041
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-10 21:21:57 +0800

    hari 70
    method parameter

---
+++ Day70.java
@@ -0,0 +1,10 @@
+
+public class serah{
+         public void ket(String nama, String ranah){
+                      System.out.println("namaku "+nama+", aku telah mencapai ranah "+ranah);
+             }
+         public static void main (String[]args){
+             serah terserah = new serah();
+                  terserah.ket("wahyu","dewa tertinggi");
+    }
+}
