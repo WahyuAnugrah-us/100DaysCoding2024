1f6935d15712ae0a3e5211a1ab497fa870d6746c
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-14 20:53:05 +0800

    hari 74
    String method : length()

---
+++ Day74.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class serah_2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan kata :");
+        String a = sc.nextLine();
+        int b = a.length();
+        System.out.print(b);
+        
+    }
+}
