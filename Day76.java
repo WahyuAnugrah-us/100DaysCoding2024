4dbf7eed57b63cd54749362ef866999f93cf1fec
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-16 23:26:32 +0800

    hari 76
    String method:toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,10 @@
+
+public class serah_3 {
+    public static void main(String[] args) {
+        String x = "kamehameha";
+        String teksUpperCase = x.toUpperCase();
+        System.out.println("untuk mengeluarkan kamehameha kamu harus mengucapkannya : " + x);
+        System.out.println("terik blok : " + teksUpperCase);
+        System.out.println("awokawokawok percaya aja si d*ng*");
+    }
+}
