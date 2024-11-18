4f4628a7835010e0b227ed5f6865caba24a22035
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-18 21:51:18 +0800

    hari 78
    String method : equalsInnoreCase

---
+++ Day78.java
@@ -0,0 +1,10 @@
+
+public class serah_3 {
+    public static void main(String[] args) {
+        String x = "INforMaTikA";
+        String y = "inFoRmAtiKa";
+
+        System.out.print(x.equalsIgnoreCase(y));
+            
+    }
+}
