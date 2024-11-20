5ddaf9ff77844c799a82d07e678b7707cdee5d64
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-20 22:15:02 +0800

    Hari 80
    String method : trim()

---
+++ Day80.java
@@ -0,0 +1,10 @@
+
+class serah_3 {
+    public static void main(String[]args){
+        String a = "  apa ajalah  ";
+        String b = a.trim();
+        
+        System.out.println("original :'"+a+"'");
+        System.out.println("trim :'"+b+"'");
+    }
+}
