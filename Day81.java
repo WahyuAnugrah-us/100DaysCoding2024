3eb1025d4067c2fe4af6313712754935346f8d4d
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-21 20:42:50 +0800

    hari 81
    String method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,10 @@
+
+class serah_3 {
+    public static void main(String[]args){
+        int a = 700;
+        String b = String.valueOf(a);
+        
+        System.out.println(a+50+50);
+        System.out.println(b+50+50);
+    }
+}
