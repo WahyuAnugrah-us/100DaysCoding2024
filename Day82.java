5a81e11d54137baf71cff1695a954be138e31be3
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-22 22:37:41 +0800

    hari 82
    method ceil(), floor(), ground()

---
+++ Day82.java
@@ -0,0 +1,9 @@
+
+class serah_3 {
+    public static void main(String[]args){
+        System.out.println(Math.ceil(4.2));
+        System.out.println(Math.floor(4.8));
+        System.out.println(Math.round(4.5));
+        System.out.println(Math.round(4.4));
+    }
+}
