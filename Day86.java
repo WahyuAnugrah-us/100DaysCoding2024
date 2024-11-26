9531e576579b75f5f4a62b5b2b28b6b0f7ba6cf8
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-26 21:32:03 +0800

    hari 86
    mencetak elemen pada array satu dimensi

---
+++ Day86.java
@@ -0,0 +1,9 @@
+
+class serah_3 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        for (int i = 0;i<angka.length;i++){
+            System.out.print(" "+angka[i]);
+        }
+    }
+}
