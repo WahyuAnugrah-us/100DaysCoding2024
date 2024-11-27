8b1a2fe049cb15be9f1bf431a7f9c459530056aa
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-11-27 19:28:11 +0800

    hari 87
    mengubah nilai array pada indeks tertentu

---
+++ Day87.java
@@ -0,0 +1,11 @@
+
+class serah_3 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        angka[0]=15;
+        angka[4]=10;
+        for (int i = 0;i<angka.length;i++){
+            System.out.print(" "+angka[i]);
+        }
+    }
+}
