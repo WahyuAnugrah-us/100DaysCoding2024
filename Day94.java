f88244390b64b3bb21df00a6fc64604965f63203
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2024-12-04 23:14:54 +0800

    hari 94
    pola segitiga

---
+++ Day94.java
@@ -0,0 +1,22 @@
+
+public class lagi {
+    public static void main(String[]args){
+        int a =5;
+        for (int i = 0;i<a;i++){
+            for (int j = 0;j<a-i-1;j++){
+            System.out.print(" ");
+            }
+            System.out.print(" ");
+            
+            for(int j =0;j<2*i-1;j++){
+                System.out.print("*");
+            }
+            
+            if(i>0){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
