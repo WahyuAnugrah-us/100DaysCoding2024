d9caf13a7b1878bc963d08ceb3ea3678d52646fe
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2025-09-08 15:04:54 +0800

    Create 2025Day1.java
    hello world dan struktur dasar code java

---
+++ 2025Day1.java
@@ -0,0 +1,8 @@
+//struktur dasar code java
+package wahyuAnugrahCommit;//package
+import java.util.*;//import
+public class day1 {//class
+    public static void main(String[] args) {//method main
+        System.out.println("hello world");
+    }
+}
