60f6a7d0a88ae33d5e91eee230ccc4bf523dd0a9
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2025-09-10 21:18:08 +0800

    2025day3.java
     Memahami dan menggunakan Komentar

---
+++ 2025day3.java
@@ -0,0 +1,32 @@
+package wahyuAnugrahCommit;
+
+public class day3 {
+    public static void main(String[] args) {
+        /**
+        * fungsi komentar apa bang
+        * jadi gini bang
+        * komentar itu tujuannya
+        * memperjelas kode .
+         * kita menulis mengapa code ini ditulis,
+        * mengapa pake algoritma ini,mengapa pake method ini.
+        * kalo suatu hari nanti kita buka kode lagi. komentar ini akan sangat membantu
+        *
+        * membantu kolaborasi .
+        * bayangin lagi bikin project kelompok.
+        * kamu buka kodingan... komentarnya gak ada. kok ada ini? kok pake ini?
+        * terpaksa nanya ke teman kelompok lagi deh. kalo ada komen kan bisa langsung tau
+         *
+         * Mempermudah Perawatan (Maintenance) .
+        * kalo kita buka kode lama. kadang kita lupa ini dulunya kubikin gini biar apa.
+        * bayangin kita mau benerin kode buat input nama. ternyata yang kita otak atik itu kode untuk nim
+        * bukannya bagus malah tambah rusak. kalo ada komentarkan gampang
+        *
+        * Alat Debugging
+        * kalo ada kode eror tinggal ita buat jadi komentar sementara untuk cari tau
+        */
+        //ini juga komen
+        /*
+        ini juga komen
+         */
+    }
+}
